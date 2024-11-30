3985441dc25dc7bc17e9efffa0c84e5906280568
Anagho <anaghosamroyce@gmail.com>
2024-11-30 10:03:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import products from "./data/products";
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mt-5">
+      <Row>
+        <h2 className="text-secondary text-center fw-bold text-uppercase mb-4">
+          Products Store
+        </h2>
+      </Row>
+      <Row className="g-4">
+        {products.map((product) => {
+          return (
+            <Col key={product.id} md={6} lg={4}>
+              <Card className="shadow">
+                {/* Card Image */}
+                <Card.Img
+                  variant="center"
+                  src={product.image}
+                  alt="Product"
+                  className="img-fluid"
+                  style={{ height: "200px", objectFit: "cover" }}
+                />
+
+                {/* Card Body */}
+                <Card.Body>
+                  <Card.Title>{product.name}</Card.Title>
+                  <Card.Text>{product.description}</Card.Text>
+                  <Card.Text className="fw-bold">{product.price}</Card.Text>
+                  <Button variant="warning" className="w-100">Buy Now</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
   );
 }
 
---
+++ src/components/Price.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Price(props) {
+  return (
+    <p>{props.price}</p>
+  )
+}
+
+export default Price
